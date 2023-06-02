7770f7c21dd861c80bd7089a3b3201322a5231f9
zhelddlek <zhelddlek@gmail.com>
2023-06-02 16:38:12 +0900

    fresh first

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom";
+import styles from "./Home.module.css";
 
 function Detail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [loading, setLoading] = useState(true);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <div>{movie.data.movie.title}</div>
+      )}
+    </div>
+  );
 }
 export default Detail;
